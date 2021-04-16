1cf206689093764dabc01f8b82eaba33d9674873
Chen (Benjamin) Hsieh <Chen.Hsieh@uga.edu>
2021-04-16 11:43:26 -0400

    argsparse

--- AgeseqMain.py
+++ AgeseqMain.py
@@ -4,6 +4,7 @@ By running this script, AGEseq will automatically run with default setting.
 """
 
 from __future__ import division
+import argparse
 from datetime import datetime
 import os
 import sys
@@ -23,7 +24,7 @@ READS_PATH = pwd / "reads"
 TEMP_TARGET_FILE = "TEMP_TARGET.fa"
 READS_FILE_LIST = list()
 DEF_BLAT_PATH = pwd / "blat"
-#record for script running
+# record for script running
 sysdatetime = datetime.now()
 dt_string = sysdatetime.strftime("%Y%m%d_%H_%M_%S")
 logfile_name = "AGESeq_run_"+dt_string+".log"
@@ -32,12 +33,13 @@ WELCOM_MESSAGE = "Thank you for using AGEseq!\n"
 mod = "legacy"
 logfile.write(WELCOM_MESSAGE)
 
-READ_SNP_MINR_FREQ = 0.05  #Target specific, not overall frequency
-READ_INDEL_MINR_FREQ = 0.01 #Target specific, not overall frequency
+READ_SNP_MINR_FREQ = 0.05  # Target specific, not overall frequency
+READ_INDEL_MINR_FREQ = 0.01  # Target specific, not overall frequency
 READ_SNP_MINIMAL_SUPPORT = 3
 READ_INDEL_MINIMAL_SUPPORT = 5
 WOBBLE_BASE = True
 WOBBLE_FREQ_RANGE = (0.35, 0.65)
+
 
 def main():
     # checking path/dir/
@@ -57,7 +59,7 @@ def main():
         read2fas_file = str(urfile)+".fa"
         read_file.toFastaFile(read2fas_file)
         # Run blat
-        blat_outfmt="psl"
+        blat_outfmt = "psl"
         os_blat = "blat"
         file_blat_in = read2fas_file
         blat_out = f'{file_blat_in}_blat_crispr.psl'
@@ -80,18 +82,20 @@ def main():
                                               target_assigned_count)
         logfile.write("Assignment begins!"+"\n")
 
-        #issue add comment
+        # issue add comment
         file_assign_sum = assign_mask(assigned_core, mismatch_collection,
-                    indel_collection, target_assigned_count, WOBBLE_BASE)
-        #issue add comment
+                                      indel_collection, target_assigned_count, WOBBLE_BASE)
+        # issue add comment
         for t in file_assign_sum:
             for ep in file_assign_sum[t]:
                 logfile.write(t+"\t"+ep+"\t"+str(file_assign_sum[t][ep])+"\n")
-        #issue add comment
+        # issue add comment
         for eachtarget in sorted(target_assigned_count):
-            target_assigned_prop = round(target_assigned_count[eachtarget]/len(assigned_core)*100, 1)
-            logfile.write("\n"+eachtarget+"\t"+str(target_assigned_count[eachtarget])+"\t"+
+            target_assigned_prop = round(
+                target_assigned_count[eachtarget]/len(assigned_core)*100, 1)
+            logfile.write("\n"+eachtarget+"\t"+str(target_assigned_count[eachtarget])+"\t" +
                           str(target_assigned_prop)+"% of assigned reads"+"\n")
+
 
 def cal_target_var_freq(mismatch_collection, indel_collection,
                         target_assigned_count):
@@ -128,19 +132,22 @@ def assign_mask(assigned_core, mismatch_collection,
                 misfreq = round(totalmismatch/target_assigned_count[coreBH], 3)
                 if misfreq < READ_SNP_MINR_FREQ or totalmismatch < READ_SNP_MINIMAL_SUPPORT:
                     var_mask[mismatch] = "masked"
-                    logfile.write(str_mismatch+"\t"+str(misfreq)+"\t"+
+                    logfile.write(str_mismatch+"\t"+str(misfreq)+"\t" +
                                   " removed!\n")
                 else:
                     if WOBBLE_BASE:
                         target_range = as_core.getHSPrange()
                         mispos = int(mismatch.split(":")[0])
-                        if (mispos -target_range[0] < 12 or abs(mispos -target_range[1]) <12) and (misfreq < WOBBLE_FREQ_RANGE[1] and misfreq > WOBBLE_FREQ_RANGE[0]):
+                        if (mispos - target_range[0] < 12 or abs(mispos - target_range[1]) < 12) and (misfreq < WOBBLE_FREQ_RANGE[1] and misfreq > WOBBLE_FREQ_RANGE[0]):
                             var_mask[mismatch] = "masked"
-                            logfile.write(str_mismatch+"\t"+str(misfreq)+" masked as a wobblebase!\n")
+                            logfile.write(
+                                str_mismatch+"\t"+str(misfreq)+" masked as a wobblebase!\n")
                         else:
-                            logfile.write(str_mismatch+"\t"+str(misfreq)+" kept!\n")
+                            logfile.write(str_mismatch+"\t" +
+                                          str(misfreq)+" kept!\n")
                     else:
-                        logfile.write(str_mismatch+"\t"+str(misfreq)+" kept!\n")
+                        logfile.write(str_mismatch+"\t" +
+                                      str(misfreq)+" kept!\n")
         if len(coreINDEL) > 0:
 
             logfile.write(str(len(coreINDEL))+" INDEL(s) is/are found!\n")
@@ -150,7 +157,7 @@ def assign_mask(assigned_core, mismatch_collection,
                 indelfreq = round(totalindel/target_assigned_count[coreBH], 3)
                 if indelfreq < READ_INDEL_MINR_FREQ or totalindel < READ_INDEL_MINIMAL_SUPPORT:
                     var_mask[indel] = "masked"
-                    logfile.write(indel+"\t"+str(indelfreq)+"\t"+
+                    logfile.write(indel+"\t"+str(indelfreq)+"\t" +
                                   " removed!\n")
                 else:
                     logfile.write(indel+"\t"+str(indelfreq)+" kept!\n\n")
@@ -160,7 +167,7 @@ def assign_mask(assigned_core, mismatch_collection,
         as_core.assignEditPat()
         coreEP = as_core.getEditPat()
         if type(coreEP) is list:
-            #issue
+            # issue
             coreEP = coreEP[0]
         if coreBH in file_summary:
             if coreEP in file_summary[coreBH]:
@@ -172,8 +179,10 @@ def assign_mask(assigned_core, mismatch_collection,
         logfile.write(str(coreEP)+" assigned!\n\n")
     return file_summary
 
+
 def masked_edit_pattern(assigned_core):
     assigned_core
+
 
 def psl_parse(read_file, target_file, blat_out):
     sample_readslist = read_file.getallreadsids()
@@ -186,7 +195,8 @@ def psl_parse(read_file, target_file, blat_out):
     number_q_in_psl = len(psl_dict)
     perc_hit = number_q_in_psl/number_reads_in_sample*100
     #to2file = open("test_pa.out", "w")
-    logfile.write("%i reads have hits to the target sequences provided (%d%% of the sample)!" % (number_q_in_psl, perc_hit)+"\n")
+    logfile.write("%i reads have hits to the target sequences provided (%d%% of the sample)!" % (
+        number_q_in_psl, perc_hit)+"\n")
     for qid in psl_dict:
         ASCore_collection[qid] = AgeseqIO.ASCore(qid)
         psl_t_dict = dict()
@@ -199,7 +209,8 @@ def psl_parse(read_file, target_file, blat_out):
                         q_besthit[qid] = hit.id
                     elif hsp.score == q_besthit_score[qid]:
                         logfile.write(qid+" Tie Hit found!"+str(hit.id)+"\n")
-                        logfile.write(qid+" Tie Hit found!"+q_besthit[qid]+"\n")
+                        logfile.write(qid+" Tie Hit found!" +
+                                      q_besthit[qid]+"\n")
                         tier_count += 1
                     else:
                         continue
@@ -207,7 +218,7 @@ def psl_parse(read_file, target_file, blat_out):
                     q_besthit_score[qid] = hsp.score
                     q_besthit[qid] = hit.id
         ASCore_collection[qid].updatePSLTargets(psl_t_dict)
-    #for eachread in sample_readslist:
+    # for eachread in sample_readslist:
     print("Found %i reads have tie hit" % tier_count)
     PATTERN_COLLECTION = defaultdict(dict)
     MISMATCH_COLLECTION = defaultdict(dict)
@@ -223,9 +234,9 @@ def psl_parse(read_file, target_file, blat_out):
             target_assigned_count[q_besthit[query]] += 1
         else:
             target_assigned_count[q_besthit[query]] = 1
-        #logfile.write(str(query)+"\t"+q_besthit[query]+
+        # logfile.write(str(query)+"\t"+q_besthit[query]+
         #              "\t"+str(q_besthit_score[query])+"\n")
-        #to2file.write(str(ASCore_collection[query].getPSLTargets())+"\n")
+        # to2file.write(str(ASCore_collection[query].getPSLTargets())+"\n")
         for ehsp in q_hsp:
             q_start_all = ehsp.query_start_all
             q_span_all = ehsp.query_span_all
@@ -251,7 +262,8 @@ def psl_parse(read_file, target_file, blat_out):
             if strand_info[0] == "-":
                 ucount = 0
                 for seqs in query_frag_seq:
-                    query_frag_seq[ucount]=Seq.reverse_complement(query_frag_seq[ucount])
+                    query_frag_seq[ucount] = Seq.reverse_complement(
+                        query_frag_seq[ucount])
                     ucount += 1
         psl_str = str("*"*ehsp.gapopen_num+str(q_start_all)+" "+str(q_span_all)
                       + " "+str(h_start_all)+" "+str(h_span_all)+" "+str(q_gap)
@@ -268,13 +280,16 @@ def psl_parse(read_file, target_file, blat_out):
                 else:
                     blast_link.append(".")
                     mismatch_pos = h_start_all[i] + j + 1
-                    MISMATCH[mismatch_count] = str(mismatch_pos) + ":" + str(hit_frag_seq[i][j]) + "->"+str(query_frag_seq[i][j])
+                    MISMATCH[mismatch_count] = str(
+                        mismatch_pos) + ":" + str(hit_frag_seq[i][j]) + "->"+str(query_frag_seq[i][j])
                     if MISMATCH[mismatch_count] in MISMATCH_COLLECTION[q_besthit[query]]:
-                        MISMATCH_COLLECTION[q_besthit[query]][MISMATCH[mismatch_count]] += 1
+                        MISMATCH_COLLECTION[q_besthit[query]
+                                            ][MISMATCH[mismatch_count]] += 1
                     else:
-                        MISMATCH_COLLECTION[q_besthit[query]][MISMATCH[mismatch_count]] = 1
+                        MISMATCH_COLLECTION[q_besthit[query]
+                                            ][MISMATCH[mismatch_count]] = 1
                     mismatch_count += 1
-            #logfile.write(str(hit_frag_seq[i])+"\n")
+            # logfile.write(str(hit_frag_seq[i])+"\n")
             #logfile.write(str(query_frag_seq[i]) +"\n")
             #logfile.write(str("".join(blast_link)) + "\n\n")
         #to2file.write(str(MISMATCH) + "\n\n")
@@ -291,7 +306,8 @@ def psl_parse(read_file, target_file, blat_out):
                 del_end = h_start_all[i+1] - 1
                 del_size = del_end - del_start + 1
                 del_seq = t_seq[del_start: del_start+del_size]
-                del_pat = str(del_start) + "D" + str(del_size) + ":" + str(del_seq)
+                del_pat = str(del_start) + "D" + \
+                    str(del_size) + ":" + str(del_seq)
                 ASCore_collection[query].addINDEL(del_pat)
                 if del_pat in PATTERN_COLLECTION[q_besthit[query]]:
                     PATTERN_COLLECTION[q_besthit[query]][del_pat] += 1
@@ -306,9 +322,10 @@ def psl_parse(read_file, target_file, blat_out):
                 if strand_info[0] == "-":
                     ins_start_onquery = q_start_all[i+1] + q_span_all[i+1]
                     ins_end_onquery = q_start_all[i] - 1
-                ins_size = ins_end_onquery - ins_start_onquery +1
+                ins_size = ins_end_onquery - ins_start_onquery + 1
                 ins_seq = q_seq[ins_start_onquery: ins_start_onquery+ins_size]
-            ins_pat = str(ins_start_ontarget) + "I" + str(ins_size) + ":" + str(ins_seq)
+            ins_pat = str(ins_start_ontarget) + "I" + \
+                str(ins_size) + ":" + str(ins_seq)
             ASCore_collection[query].addINDEL(ins_pat)
             if ins_pat in PATTERN_COLLECTION[q_besthit[query]]:
                 PATTERN_COLLECTION[q_besthit[query]][ins_pat] += 1
@@ -316,6 +333,8 @@ def psl_parse(read_file, target_file, blat_out):
                 PATTERN_COLLECTION[q_besthit[query]][ins_pat] = 1
             #logfile.write(ins_pat + "\n\n")
     return ASCore_collection, MISMATCH_COLLECTION, PATTERN_COLLECTION, target_assigned_count
+
+
 '''
     for eachtarget in sorted(target_assigned_count):
         target_assigned_prop = round(target_assigned_count[eachtarget]/number_q_in_psl*100, 1)
@@ -333,8 +352,8 @@ def psl_parse(read_file, target_file, blat_out):
             to2file.write(hit+"\t"+pat+"\t"+str(MISMATCH_COLLECTION[hit][pat])+
                           "\t"+mis_freq+"\n")
 '''
-    # Write output
-    # Clean up
+# Write output
+# Clean up
 
 
 def check_conf():
@@ -378,45 +397,64 @@ def check_blat():
     print("AGEseq will run on a "+user_os+" machine!")
     return DEF_BLAT_PATH
 
+
 def load_target():
-    #Load target
-    v1_targetfile= pwd / "targets.txt"
+    # Load target
+    v1_targetfile = pwd / "targets.txt"
     if os.path.exists(v1_targetfile):
         print("Target File is found:" + str(v1_targetfile))
         UTarget = AgeseqIO.readv1TargetFile(v1_targetfile)
-        #UTarget.showAsFasta()
+        # UTarget.showAsFasta()
         UTarget.toFastaFile(TEMP_TARGET_FILE)
         return UTarget
     else:
         print("Couldn't locate target file:"+str(v1_targetfile))
         exit
 
+
 def get_reads_file():
     if os.path.isdir(READS_PATH):
         print("Found Reads Directory: " + str(READS_PATH) + " successfully!")
         import glob
         READS_FILE_LIST = glob.glob(os.path.join(READS_PATH, '*'))
-        
+
         return READS_FILE_LIST
     else:
         print(str(READS_PATH) + "doesn't exists")
         exit
 
+
 HELP = ""
-import argparse
 if __name__ == '__main__':
 
-
+    # ageseq.py \
+    # -t target.fa \
+    # -r /dir/of/reads/ \
+    # --READ_SNP_MINR_FREQ 0.05 \
+    # --READ_INDEL_MINR_FREQ 0.01 \
+    # --READ_SNP_MINIMAL_SUPPORT 3 \
+    # --READ_INDEL_MINIMAL_SUPPORT 5 \
+    # --WOBBLE_BASE "true" \
+    # --WOBBLE_FREQ_RANGE 0.35 0.65
     parser = argparse.ArgumentParser(description='AGEseq2')
-    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
-    #                     help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
-
+    parser.add_argument('target_file', metavar='t', type=str,
+                        help='fasta format target file')
+    parser.add_argument('directory of reads', metavar='r', type=str,
+                        help='fasta/fastq format reads dir')
+    parser.add_argument('--READ_SNP_MINR_FREQ', dest='READ_SNP_MINR_FREQ', type=float,
+                        help='')
+    parser.add_argument('--READ_INDEL_MINR_FREQ', dest='READ_INDEL_MINR_FREQ', type=float,
+                        help='')
+    parser.add_argument('--READ_SNP_MINIMAL_SUPPORT', dest='READ_SNP_MINIMAL_SUPPORT', type=float,
+                        help='')
+    parser.add_argument('--READ_INDEL_MINIMAL_SUPPORT', dest='READ_INDEL_MINIMAL_SUPPORT', type=float,
+                        help='')
+    parser.add_argument('--WOBBLE_BASE', dest='WOBBLE_BASE', type=float,
+                        help='')
+    parser.add_argument('--WOBBLE_FREQ_RANGE', dest='WOBBLE_FREQ_RANGE', nargs=2, type=float,
+                        help='')
     args = parser.parse_args()
     # print(args.accumulate(args.integers))
-
 
     main()
     print(HELP + "\n")
